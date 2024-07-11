cd788662befb4d83ea6875088cb6299c99e74294
yb0x00 <yeongbeen0x00@gmail.com>
2024-07-12 04:27:34 +0900

    feat: Enable search bar to open search screen on click
    [mystrings.xml]
    - hint에 띄울 문구 추가
    [search_icon.xml]
    - 검색 버튼 이미지 추가
    [activity_home_map.xml]
    - UI에 검색창 추가
    [HomeMapActivity.kt]
    - 검색창을 선택하면 검색 화면으로 이동

--- app/src/main/java/campus/tech/kakao/map/HomeMapActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/HomeMapActivity.kt
@@ -1,9 +1,12 @@
 package campus.tech.kakao.map
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.kakao.vectormap.KakaoMap
@@ -16,7 +19,9 @@ import java.security.MessageDigest
 
 class HomeMapActivity : AppCompatActivity() {
     lateinit var mapView: MapView
+    lateinit var searchBar: EditText
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -38,6 +43,11 @@ class HomeMapActivity : AppCompatActivity() {
             }
 
         })
+        searchBar = findViewById(R.id.search_home)
+        searchBar.setOnClickListener {
+            val intent = Intent(this, DataSearchActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onResume() {
